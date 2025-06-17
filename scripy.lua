6e7b390ab0cb0cf4ccb2488dd6abe17f10819c92
Daniil Rot <shprotokot@gmail.com>
2025-06-18 01:27:20 +0300

    Update scripy.lua

--- scripy.lua
+++ scripy.lua
@@ -1,8 +1,74 @@
-print("Привет, мир!")
+-- RPG бой на Lua
 
-local sum = 0
-for i = 1, 10 do
-    sum = sum + i
+-- Игрок
+local player = {
+    name = "Герой",
+    hp = 100,
+    maxHp = 100,
+    attackMin = 10,
+    attackMax = 20,
+    healAmount = 15
+}
+
+-- Монстр
+local monster = {
+    name = "Монстр",
+    hp = 80,
+    maxHp = 80,
+    attackMin = 8,
+    attackMax = 18
+}
+
+-- Функция атаки
+local function attack(attacker, defender)
+    local damage = math.random(attacker.attackMin, attacker.attackMax)
+    defender.hp = math.max(defender.hp - damage, 0)
+    print(attacker.name .. " наносит " .. damage .. " урона " .. defender.name)
 end
 
-print("Сумма чисел от 1 до 10 равна:", sum)
+-- Функция лечения
+local function heal(character)
+    character.hp = math.min(character.hp + character.healAmount, character.maxHp)
+    print(character.name .. " восстанавливает " .. character.healAmount .. " здоровья")
+end
+
+-- Проверка жив ли персонаж
+local function isAlive(character)
+    return character.hp > 0
+end
+
+-- Игровой цикл
+math.randomseed(os.time())
+
+print("Начинается битва!")
+while isAlive(player) and isAlive(monster) do
+    print("\nТвой ход! Выбери действие:")
+    print("1. Атаковать")
+    print("2. Лечиться")
+    io.write("> ")
+    local choice = io.read()
+
+    if choice == "1" then
+        attack(player, monster)
+    elseif choice == "2" then
+        heal(player)
+    else
+        print("Некорректный ввод, пропускаешь ход!")
+    end
+
+    if not isAlive(monster) then
+        print("\nМонстр повержен! Ты победил!")
+        break
+    end
+
+    -- Ход монстра
+    print("\nХод монстра...")
+    attack(monster, player)
+
+    if not isAlive(player) then
+        print("\nТы пал в бою. Игра окончена.")
+    else
+        print("Твоё здоровье: " .. player.hp .. "/" .. player.maxHp)
+        print("Здоровье монстра: " .. monster.hp .. "/" .. monster.maxHp)
+    end
+end
